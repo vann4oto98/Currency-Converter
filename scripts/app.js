190907d2f81a66f2e96fec31476f21b155483332
aleksandar-petrov <mr.aleksandar.petrov@gmail.com>
2020-07-06 12:19:16 +0300

    refactor

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 import '../scripts/services/user-service.js';
 import '../scripts/services/currency-service.js';
 import '../scripts/services/dom-service.js';
+import '../scripts/nav/nav.js';
 
 
 
--- scripts/table.js
+++
@@ -1,9 +0,0 @@
-import { requester } from "../utils/kinvey-requester.js";
-//todo  (i have no access to database)
-function fillTable() {
-  requester.get("appdata", "currency", "Kinvey").then((currency) => {
-    console.log(currency);
-  });
-}
-
-//fillTable();
