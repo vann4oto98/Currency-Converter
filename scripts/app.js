351afebc557661601ad08c19fcdbfb53d72365e1
aleksandar-petrov <mr.aleksandar.petrov@gmail.com>
2020-07-06 12:35:23 +0300

    refactor

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 import '../scripts/services/user-service.js';
 import '../scripts/services/currency-service.js';
 import '../scripts/services/dom-service.js';
 import './utils/nav.js';
-
-
-
-=======
-import "../scripts/services/user-service.js";
-import "../scripts/services/currency-service.js";
-import "../scripts/services/dom-service.js";
-import "./utils/nav.js";
->>>>>>> a3dec684930ed85f416abff042456706bbaa80da
