14c2d56a980ce045509d87d35bf7cb2a47cfdf0d
aleksandar-petrov <mr.aleksandar.petrov@gmail.com>
2020-07-17 23:09:08 +0300

    Added Fill Previous Dates Functionality

--- scripts/services/currency-service.js
+++ scripts/services/currency-service.js
@@ -19,31 +19,36 @@ export function init() {
             mainRatesSource.dates = JSON.parse(JSON.stringify(datesArray));
             mainRatesSource.dates.sort((a, b) => b.localeCompare(a));
             mainRatesSource.chosenDate = currentDate;
-
             if (!datesArray.includes(currentDate)) {
-                requester.post("appdata", "dates", {
-                        added_date: currentDate
-                    }, "Kinvey")
-                    .then(response => {
+                fillPreviousDates(mainRatesSource.dates[0], currentDate)
+                    .then(promiseAllValues => {
 
-                    }).catch(console.error);
+                        mainRatesSource.dates = getNewDatesArrayWithAddedDates(promiseAllValues, mainRatesSource.dates);
 
-                fetch('https://api.exchangeratesapi.io/latest')
-                    .then(response => response.json())
-                    .then(data => {
-                        requester.post("appdata", "rates", {
-                                date: currentDate,
-                                rates: data.rates
+                        requester.post("appdata", "dates", {
+                                added_date: currentDate
                             }, "Kinvey")
                             .then(response => {
-                                data.rates['EUR'] = 1;
-                                mainRatesSource.rates = data.rates;
+                                fetch('https://api.exchangeratesapi.io/latest')
+                                    .then(response => response.json())
+                                    .then(data => {
+                                        requester.post("appdata", "rates", {
+                                                date: currentDate,
+                                                rates: data.rates
+                                            }, "Kinvey")
+                                            .then(response => {
+                                                data.rates['EUR'] = 1;
+                                                mainRatesSource.dates.unshift(currentDate);
+                                                mainRatesSource.rates = data.rates;
 
-                                domInit(mainRatesSource);
-                            })
-                            .catch(console.error);
+                                                domInit(mainRatesSource);
+                                            })
+                                            .catch(console.error);
+                                    });
+
+                            }).catch(console.error);
+
                     });
-
             } else {
                 getRatesByDate(currentDate)
                     .then(data => {
@@ -63,6 +68,73 @@ function getRatesByDate(date) {
 
 }
 
+function fillPreviousDates(latestDate, currentDate) {
+
+    const preCurrentDate = getDateWithDaysDifference(currentDate, -1);
+    const postLatestDate = getDateWithDaysDifference(latestDate, 1)
+
+
+    return fetch(`https://api.exchangeratesapi.io/history?start_at=${postLatestDate}&end_at=${preCurrentDate}`)
+        .then(x => x.json())
+        .then(x => {
+            const ratesToFill = [];
+
+            Object.keys(x.rates)
+                .forEach(key => ratesToFill.push({
+                    date: key,
+                    rates: x.rates[key]
+                }))
+
+
+            ratesToFill.sort((a, b) => a.date.localeCompare(b.date));
+
+            return ratesToFill;
+        })
+        .then(x => {
+            const requestsArray = [];
+
+            x.forEach(rateElement => {
+                requestsArray.push(makeDateCreateRequest(rateElement.date));
+                requestsArray.push(makeRateCreateRequest(rateElement.date, rateElement.rates));
+            })
+
+            return requestsArray;
+        }).then(x => Promise.all(x));
+}
+
+function getNewDatesArrayWithAddedDates(promiseAllValues, datesArray) {
+
+    const newArray = datesArray.slice(0);
+
+    for (let i = 0; i < promiseAllValues.length; i+=2) {
+        
+        newArray.unshift(promiseAllValues[i].added_date);
+    }
+
+    return newArray;
+
+}
+
+function getDateWithDaysDifference(currentDate, difference) {
+    let lastDate = new Date(Date.parse(currentDate));
+    lastDate.setDate(lastDate.getDate() + difference);
+    lastDate = lastDate.toJSON().slice(0, 10);
+    return lastDate;
+}
+
+function makeDateCreateRequest(date) {
+    return requester.post("appdata", "dates", {
+        added_date: date
+    }, "Kinvey");
+}
+
+function makeRateCreateRequest(date, rates) {
+    return requester.post("appdata", "rates", {
+        date: date,
+        rates: rates
+    }, "Kinvey");
+}
+
 export function getRatesSourceByDate(date) {
 
     const ratesSource = {
@@ -77,20 +149,23 @@ export function getRatesSourceByDate(date) {
         });
 }
 
-export function getTheLastTenRates (){
+export function getTheLastTenRates() {
 
     return requester.get("appdata", "rates", "Kinvey", `?query={}&sort={"date": -1}&limit=10`)
         .then(allDates => {
 
             const finalRates = allDates.map(x => {
                 x.rates['EUR'] = 1;
-                return {date: x.date, rates: x.rates}
+                return {
+                    date: x.date,
+                    rates: x.rates
+                }
             });
 
             finalRates.sort((a, b) => {
                 return a.date.localeCompare(b.date);
             });
-            
+
             return finalRates;
         })
 
